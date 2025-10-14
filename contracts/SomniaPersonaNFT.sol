1896ed0cbbb91a5c4491c1fdc001a23a4f390a02
RivalismeUniverse <unuser.v01@gmail.com>
2025-10-15 03:06:18 +0700

    Update SomniaPersonaNFT.sol

--- contracts/SomniaPersonaNFT.sol
+++ contracts/SomniaPersonaNFT.sol
@@ -1,70 +1,133 @@
-```solidity
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.9.3/contracts/access/Ownable.sol";
 
-/**
- * @title SomniaPersonaNFT
- * @dev NFT contract for AI Personas in RivalismeUniverse
- */
-contract SomniaPersonaNFT is ERC721, Ownable {
-    uint256 public tokenIdCounter;
-    
+contract SomniaPersonaNFT is ERC721, ERC721URIStorage, Ownable {
+    uint256 private _tokenIdCounter;
+
     struct Persona {
         string name;
         string specialization;
-        string metadataURI;
         uint256 mintedAt;
+        address creator;
     }
-    
+
     mapping(uint256 => Persona) public personas;
     mapping(address => uint256[]) public userPersonas;
-    
-    event PersonaMinted(
-        address indexed owner,
-        uint256 indexed tokenId,
-        string name,
-        string specialization
-    );
 
-    constructor() ERC721("SomniaPersona", "SPNFT") Ownable(msg.sender) {}
+    event PersonaMinted(address indexed owner, uint256 indexed tokenId, string name, string specialization);
+    event PersonaUpdated(uint256 indexed tokenId, string newURI);
+
+    constructor() ERC721("RivalismeUniverse Persona", "RVPERSONA") {
+        _tokenIdCounter = 0;
+    }
 
     function mintPersona(
-        string calldata name,
-        string calldata spec,
-        string calldata uri
-    ) external returns (uint256) {
-        uint256 tokenId = tokenIdCounter;
-        tokenIdCounter++;
-        
-        _safeMint(msg.sender, tokenId);
-        
+        address to,
+        string memory name,
+        string memory specialization,
+        string memory uri
+    ) public returns (uint256) {
+        require(bytes(name).length > 0, "Name cannot be empty");
+        require(bytes(specialization).length > 0, "Specialization cannot be empty");
+        require(bytes(uri).length > 0, "URI cannot be empty");
+
+        uint256 tokenId = _tokenIdCounter;
+        _tokenIdCounter++;
+
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+
         personas[tokenId] = Persona({
             name: name,
-            specialization: spec,
-            metadataURI: uri,
-            mintedAt: block.timestamp
+            specialization: specialization,
+            mintedAt: block.timestamp,
+            creator: msg.sender
         });
-        
-        userPersonas[msg.sender].push(tokenId);
-        
-        emit PersonaMinted(msg.sender, tokenId, name, spec);
+
+        userPersonas[to].push(tokenId);
+
+        emit PersonaMinted(to, tokenId, name, specialization);
+
         return tokenId;
     }
 
-    function getPersonasByOwner(address owner) external view returns (uint256[] memory) {
+    function mintMyPersona(
+        string memory name,
+        string memory specialization,
+        string memory uri
+    ) public returns (uint256) {
+        return mintPersona(msg.sender, name, specialization, uri);
+    }
+
+    function getPersonasByOwner(address owner)
+        public
+        view
+        returns (uint256[] memory)
+    {
         return userPersonas[owner];
     }
 
-    function tokenURI(uint256 tokenId) public view override returns (string memory) {
-        require(ownerOf(tokenId) != address(0), "Token does not exist");
-        return personas[tokenId].metadataURI;
+    function getPersona(uint256 tokenId)
+        public
+        view
+        returns (
+            string memory name,
+            string memory specialization,
+            uint256 mintedAt,
+            address creator,
+            address currentOwner
+        )
+    {
+        require(_ownerOf(tokenId) != address(0), "Persona does not exist");
+
+        Persona memory p = personas[tokenId];
+        return (
+            p.name,
+            p.specialization,
+            p.mintedAt,
+            p.creator,
+            ownerOf(tokenId)
+        );
     }
 
-    function getPersona(uint256 tokenId) external view returns (Persona memory) {
-        require(ownerOf(tokenId) != address(0), "Token does not exist");
-        return personas[tokenId];
+    function updateTokenURI(uint256 tokenId, string memory newURI) public {
+        require(ownerOf(tokenId) == msg.sender, "Not the owner");
+        _setTokenURI(tokenId, newURI);
+        emit PersonaUpdated(tokenId, newURI);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _tokenIdCounter;
+    }
+
+    function personaExists(uint256 tokenId) public view returns (bool) {
+        return _ownerOf(tokenId) != address(0);
+    }
+
+    // Overrides
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
 }
