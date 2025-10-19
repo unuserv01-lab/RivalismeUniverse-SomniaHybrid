2cc584fba7198bc9b638f3435819586320e2a8fb
RivalismeUniverse <unuser.v01@gmail.com>
2025-10-19 15:27:11 +0700

    Create ipfs.js

---
+++ config/ipfs.js
@@ -0,0 +1,9 @@
+module.exports = {
+  gateways: [
+    'https://ipfs.io/ipfs/',
+    'https://gateway.pinata.cloud/ipfs/',
+    'https://cloudflare-ipfs.com/ipfs/'
+  ],
+  uploadService: 'web3.storage', 'nft.storage'
+  baseURI: 'ipfs://QmRivalismeUniverse'
+};
