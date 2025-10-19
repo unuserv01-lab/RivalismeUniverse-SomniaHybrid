7cb99c18b32f8204467ae050a8c92ab894206b06
RivalismeUniverse <unuser.v01@gmail.com>
2025-10-19 15:26:11 +0700

    Create contract.js

---
+++ config/contract.js
@@ -0,0 +1,10 @@
+module.exports = {
+  personaContract: {
+    address: '0xd9145CCE52D386f254917e481eB44e9943F39138',
+    abi: require('../contracts/RivalismePersona.json').abi
+  },
+  certificateContract: {
+    address: '0x...', // Your certificate contract address
+    abi: require('../contracts/RivalismeCertificate.json').abi
+  }
+};
