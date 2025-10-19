50c4741dad0306f8c834e09dd9aa0bab847fbcd9
RivalismeUniverse <unuser.v01@gmail.com>
2025-10-19 15:24:24 +0700

    Create network.js

---
+++ config/network.js
@@ -0,0 +1,15 @@
+module.exports = {
+  somniaTestnet: {
+    chainId: 50312,
+    chainIdHex: '0xC488',
+    name: 'Somnia Testnet',
+    rpcUrl: 'https://dream-rpc.somnia.network/',
+    explorer: 'https://shannon-explorer.somnia.network',
+    nativeCurrency: {
+      name: 'STT',
+      symbol: 'STT',
+      decimals: 18
+    }
+  },
+  // Add other networks as needed
+};
